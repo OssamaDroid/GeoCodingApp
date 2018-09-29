4117c2aedf9a58b24066651d2022c475d57778a3
OssamaDroid <bounnite.ossama@gmail.com>
2018-09-29 10:31:33 +0100

    Creating the City entity

---
+++ app/src/main/java/com/ossama/apps/geocodingapp/model/entity/City.kt
@@ -0,0 +1,9 @@
+package com.ossama.apps.geocodingapp.model.entity
+
+
+data class City(
+        var cityName: String? = null,
+        var latitude: String? = null,
+        var longitude: String? = null,
+        var countryCode: String? = null
+)
