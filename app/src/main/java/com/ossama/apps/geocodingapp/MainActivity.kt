d584df8a027b537bc9cef9e519f2b6d3cf62c6fc
OssamaDroid <bounnite.ossama@gmail.com>
2018-09-29 10:11:30 +0100

    Adding the search view to the activity

--- app/src/main/java/com/ossama/apps/geocodingapp/MainActivity.kt
+++ app/src/main/java/com/ossama/apps/geocodingapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.ossama.apps.geocodingapp
 
-import android.support.v7.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            // Make the searchView expand on the whole toolbar
+            this.maxWidth = Integer.MAX_VALUE
+        }
+
+        return true
     }
 }
