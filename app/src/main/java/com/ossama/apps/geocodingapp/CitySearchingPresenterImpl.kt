5a63b33d61de601fd1022dabfc622db3e3c0b839
OssamaDroid <bounnite.ossama@gmail.com>
2018-09-29 12:40:36 +0100

    Presenter Implementation for cities fetching

--- app/src/main/java/com/ossama/apps/geocodingapp/CitySearchingPresenterImpl.kt
+++ app/src/main/java/com/ossama/apps/geocodingapp/CitySearchingPresenterImpl.kt
@@ -1,17 +1,50 @@
 package com.ossama.apps.geocodingapp
 
+import com.ossama.apps.geocodingapp.model.entity.City
+import com.ossama.apps.geocodingapp.model.repository.CitySearchingCallback
+import com.ossama.apps.geocodingapp.model.repository.CitySearchingRepository
+import com.ossama.apps.geocodingapp.model.repository.CitySearchingRepositoryImpl
+
 
 class CitySearchingPresenterImpl : ICitySearchingPresenter {
 
+    private var citySearchingView: ICitySearchingView? = null
+    private val citySearchingRepository: CitySearchingRepository by lazy {
+        CitySearchingRepositoryImpl()
+    }
+
     override fun getCities() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        citySearchingView?.getViewContext()?.let {
+
+            // Show the Progress Bar
+            citySearchingView?.showLoadingView()
+
+            // Fetch the cities
+            citySearchingRepository.getCities(it, object: CitySearchingCallback {
+                override fun onSuccess(cities: List<City>) {
+                    // Success
+                    when {
+                        // List Not empty
+                        cities.isNotEmpty() -> citySearchingView?.showCities(cities)
+
+                        // List empty
+                        else -> citySearchingView?.showEmptyState()
+                    }
+                }
+
+                override fun onFailure() {
+                    // Failure
+                    citySearchingView?.showError()
+                }
+            })
+        }
     }
 
     override fun attachView(view: ICitySearchingView) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        citySearchingView = view
     }
 
     override fun detachView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        citySearchingView = null
     }
 }
